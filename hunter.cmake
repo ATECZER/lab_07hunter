e36cd4b89bda550f4dc356b11a76b5e3303df5a8
ATECZER <91702110+ATECZER@users.noreply.github.com>
2022-05-30 23:47:58 +0300

    Update hunter.cmake

--- hunter.cmake
+++ hunter.cmake
@@ -4,9 +4,9 @@ include(hunter_download)
 include(hunter_pick_scheme)
 hunter_add_version(
 	PACKAGE_NAME
-	banking
+	mypack
 	VERSION
-	1.0.0
+	0.0.1
 	URL
 	"https://github.com/ATECZER/lab06/archive/refs/tags/v0.0.1.tar.gz"
 	SHA1
@@ -15,5 +15,5 @@ hunter_add_version(
 
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
-hunter_cacheable(my_pack)
-hunter_download(PACKAGE_NAME my_pack)
+hunter_cacheable(mypack)
+hunter_download(PACKAGE_NAME mypack)
